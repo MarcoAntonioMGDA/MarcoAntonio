44b937e8fb842dfd1e350ecdf8bc538a4a70c616
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-09-18 12:45:14 -0500

    Second Version

---
+++ includes/head.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+<link rel="stylesheet" href="css/styles.css">
+<link rel="icon" type="image/png" href="favicon.png" />
+<!-- SEO Codes -->
+<meta name="keywords" content="Marbust, Technology, Company, marco, antonio, bustillos, quiroz, marco antonio, web, full, stack, developer, husband, father, youtuber, learner, BYU, idaho, student, design, development, who, is, am, about, me, ecuador, media, designer, developer, where, mgda, going, configurar, phpmailer, y, no, morir, en, el, intento, sass, html5, js, php, css, css3, sql, python">
+<meta name="description" content="I am a Full Stack Web Developer. I work as CEO and Co-Founder of Marbust Technology Company, discover more about me in this Web Site">
+<meta name="author" content="Marbust Technology Company">
+<meta name="copyright" content="© <?php echo date("Y");?> Marbust Technology Company - All Rights Reserved">
+<script src="js/functions.js"></script>
---
+++ includes/nav.php
@@ -0,0 +1,12 @@
+<nav>
+    <ul id="ul">
+        <li><a href="home"><span><i class="fas fa-home"></i></span> Home</a></li>
+        <li><a href="about"><span><i class="fas fa-address-card"></i></span> About</a></li>
+        <li><a href="resume"><span><i class="fas fa-award"></i></span> Resume</a></li>
+        <li><a href="portafolio"><span><i class="fas fa-briefcase"></i></span> Portafolio</a></li>
+        <li><a href="contact"><span><i class="fas fa-paper-plane"></i></span> Contact</a></li>
+    </ul>
+</nav>
+<script>
+getCurrentLink();
+</script>
