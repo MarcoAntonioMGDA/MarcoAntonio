7ad0913df26e8f43b95601bc056b5123ef41f0c7
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-17 10:27:06 -0500

    Update to Marbust Framework JS 2.0

--- index.js
+++ index.js
@@ -4,17 +4,59 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const session = require('express-session');
+
+const csrf = require('csurf');
+
+const flash = require('connect-flash');
+
 const app = express();
+
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 
 app.set('views', 'views');
 
-const Controllers = require('./controllers/routes');
+const PublicRoutes = require('./routes/public');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(Controllers);
+app.use(
+    session({
+      secret: 'my secret',
+      resave: false,
+      saveUninitialized: false
+    })
+  );
 
-app.listen(3000);
+  app.use(csrfProtection);
+  //Use Flash Messages
+  app.use(flash());
+  
+  app.use((req, res, next) => {
+      res.locals.csrfToken = req.csrfToken();
+      next();
+    });
+
+app.use(PublicRoutes);
+
+app.use((error, req, res, next) => {
+    res.status(500).render('template', {
+        pageTitle: 'Error de Validación',
+        PagetoLoad: '500',
+        SocialLinks: {
+            Facebook: "https://www.facebook.com/",
+            Twitter: "https://twitter.com/",
+            WhatsApp: "https://api.whatsapp.com/send?phone=593",
+            Phone: "tel:+99999999999",
+            Instagram: "https://www.instagram.com/",
+            Youtube: "https://www.youtube.com/",
+            Email: "mailto:@",
+            Copyright: "https://Sites.MarBust.com"
+        }
+    });
+  });
+
+app.listen(80);
